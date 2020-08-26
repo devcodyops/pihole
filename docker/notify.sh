35f1faa5c7c61f7f4d49e30d25aed0dc48aa6f8c
devcodyops <devcodyops@gmail.com>
2020-08-25 20:43:46 -0400

    updated notify.sh

--- docker/notify.sh
+++ docker/notify.sh
@@ -5,7 +5,7 @@
 #Check Docker container status and write to file
 sudo docker ps -a > /home/ubuntu/dockerstatus.txt
 #
-#Email deployment status your email address
+#Email deployment status your email address (use your email address you configured in /etc/ssmtp/ssmtp.conf)
 #
 sudo mpack -s "Pihole Deployed" /home/ubuntu/dockerstatus.txt devcodyops@gmail.com
 #
